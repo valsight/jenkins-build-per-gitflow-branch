8ea906dc02647a82a0a30569289b47f622f989b7
Marvin Keller <marvin.keller@valsight.com>
2016-04-25 12:17:55 +0200

    more fixes for slash in branch name

--- src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/neoteric/jenkins/JenkinsJobManager.groovy
@@ -110,7 +110,8 @@ class JenkinsJobManager {
                 }
                 println "-------> Expected jobs:"
                 expectedJobsPerBranch.each { println "           $it" }
-                List<String> jobNamesPerBranch = jobNames.findAll { it.endsWith(branchToProcess) }
+                List<String> jobNamesPerBranch = jobNames.findAll { it.endsWith(branchToProcess.replaceAll('/', '_')) }
+
                 println "-------> Job Names per branch:"
                 jobNamesPerBranch.each { println "           $it" }
                 List<ConcreteJob> missingJobsPerBranch = expectedJobsPerBranch.findAll { expectedJob ->
@@ -123,7 +124,7 @@ class JenkinsJobManager {
 
             List<String> deleteCandidates = jobNames.findAll { it.contains(branchSuffixMatch[templateBranchToProcess]) }
             List<String> jobsToDeletePerBranch = deleteCandidates.findAll { candidate ->
-                !branchesWithCorrespondingTemplate.any { candidate.endsWith(it) }
+                !branchesWithCorrespondingTemplate.any { candidate.endsWith(it.replaceAll('/', '_')) }
             }
 
             println "-----> Jobs to delete:"
